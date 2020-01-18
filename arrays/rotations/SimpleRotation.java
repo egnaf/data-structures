3430bd507acfb8dcdcb37fada583b1fd3611b973
egnaf <egnaf@yahoo.com>
2020-01-18 16:43:03 +0300

    Add a simple rotation algorithm

---
+++ arrays/rotations/SimpleRotation.java
@@ -0,0 +1,11 @@
+public class SimpleRotation {
+
+    public static void main(String[] args) {
+        int[] array = new int[] {1, 2, 3, 4, 5}; 
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
+
