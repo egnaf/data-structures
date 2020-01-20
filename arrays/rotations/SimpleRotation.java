8f45ae057f0ffe3ddc3a00a917a182133e0efdaa
Sherzod Mamadaliev <egnaf@yahoo.com>
2020-01-20 23:36:38 +0300

    Fix code style of simple array rotation algorithm #12

--- arrays/rotations/SimpleRotation.java
+++ arrays/rotations/SimpleRotation.java
@@ -1,11 +1,18 @@
+import java.util.Arrays;
+
 public class SimpleRotation {
 
     public static void main(String[] args) {
-        int[] array = new int[] {1, 2, 3, 4, 5}; 
+        int[] array = new int[] {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(rotate(array)));
+    }
+
+    private static int[] rotate(int[] array) {
+        int[] rotatedArray = new int[array.length];
 
         for (int i = array.length - 1; i >= 0; i--) {
-            System.out.print(array[i] + " ");
+            rotatedArray[i] = array[i];
         }
+        return rotatedArray;
     }
 }
-
