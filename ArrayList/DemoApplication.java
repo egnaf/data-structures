ba85e39f9a43eac413599648fa1db8958f2930fa
egnaf <egnaf@yahoo.com>
2020-02-10 12:55:08 +0300

    Add MyLinkedList class with add, get, remove methods

---
+++ ArrayList/DemoApplication.java
@@ -0,0 +1,19 @@
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        MyList<Integer> list = new MyArrayList<>();
+        list.add(1);
+        list.add(9);
+        list.add(3);
+        list.add(4);
+        list.add(-7);
+
+        System.out.println(list); // [1, 9, 3, 4, -7]
+
+        System.out.println(list.get(2)); // 3
+
+        list.remove(2);
+
+        System.out.println(list); // [1, 9, 4, -7]
+    }
+}
